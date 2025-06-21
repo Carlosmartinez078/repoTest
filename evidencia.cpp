98c46e5ca007bbe61f940c283288366aa1031670
Carlosmartinez078 <00171125@uca.edu.sv>
2025-06-21 11:41:34 -0600

    Delete evidencia.cpp

--- evidencia.cpp
+++
@@ -1,7 +0,0 @@
-#include <iostream>
-#include "evidencia.h"
-
-void Evidencia::imprimir() {
-    cout << "Nombre de Evidencia: " << nombreEvidencia << endl;
-    cout << "Te tomara alrededor de " << tiempo << " el conseguir esta evidencia";
-}
